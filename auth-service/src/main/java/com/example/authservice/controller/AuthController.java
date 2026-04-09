05a4e5561f54e2624b06a8d00b71113225e2a2b1
Tim Choy <timmymc02@gmail.com>
2026-04-09 16:54:47 -0700

    Implement auth-service and test jwt token generation

---
+++ auth-service/src/main/java/com/example/authservice/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.example.authservice.controller;
+
+import com.example.authservice.dto.LoginRequestDTO;
+import com.example.authservice.dto.LoginResponseDTO;
+import com.example.authservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
+
+        if(tokenOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String token = tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+
+}
